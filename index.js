c1e5365f2bb36e9dddfb4f5c1715c39e869e3086
Matias1311 <83086330+Matias1311@users.noreply.github.com>
2022-07-05 19:09:28 -0500

    Banner mejorado, referencias desplegables añadidas

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function closeBanner () {
     banner.style.display = "none";
 }
+var referencesAreExpanded = true;
+function expandReferences () {
+    if (referencesAreExpanded) {
+        referencesImg.style.cssText += "-webkit-transform:rotate(180deg);-moz-transform: rotate(180deg);-ms-transform: rotate(180deg);-o-transform: rotate(180deg);transform: rotate(180deg);";
+        referencesCont.style.display = "block";
+    }
+    else {
+        referencesImg.style.cssText -= "-webkit-transform:rotate(180deg);-moz-transform: rotate(180deg);-ms-transform: rotate(180deg);-o-transform: rotate(180deg);transform: rotate(180deg);";
+        referencesCont.style.display = "none";
+    }
+
+    referencesAreExpanded = !referencesAreExpanded;
+}
--- programadores.js
+++ programadores.js
@@ -41,4 +41,17 @@ function displayInfo (number) {
 function closeBanner () {
     banner.style.display = "none";
 }
+var referencesAreExpanded = true;
+function expandReferences () {
+    if (referencesAreExpanded) {
+        referencesImg.style.cssText += "-webkit-transform:rotate(180deg);-moz-transform: rotate(180deg);-ms-transform: rotate(180deg);-o-transform: rotate(180deg);transform: rotate(180deg);";
+        referencesCont.style.display = "block";
+    }
+    else {
+        referencesImg.style.cssText -= "-webkit-transform:rotate(180deg);-moz-transform: rotate(180deg);-ms-transform: rotate(180deg);-o-transform: rotate(180deg);transform: rotate(180deg);";
+        referencesCont.style.display = "none";
+    }
+
+    referencesAreExpanded = !referencesAreExpanded;
+}
 displayInfo(1);
