0711da3c4e9bf326ed671d8aabea0018d10f52d4
Matias1311 <83086330+Matias1311@users.noreply.github.com>
2022-09-05 23:14:08 -0500

    Sobre mí añadido

---
+++ sobre-mi.js
@@ -0,0 +1,49 @@
+function closeBanner () {
+    document.getElementById("banner").style.right = "0";
+    document.getElementById("banner").style.animation = "hide 0.5s ease 0s 1 normal forwards";
+    setTimeout(() => {
+        bannerShow.style.display = "block";
+        banner.style.display = "none";
+    }, 500)
+}
+function showBanner() {
+    banner.style.right = "-440px"
+    banner.style.display = "block";
+    banner.style.animation = "slide-left 1.5s ease 0s 1 normal forwards";
+    bannerShow.style.display = "none";
+}
+var referencesAreExpanded = true;
+function expandReferences () {
+    if (referencesAreExpanded) {
+        referencesImg.style.cssText += "-webkit-transform:rotate(180deg);-moz-transform: rotate(180deg);-ms-transform: rotate(180deg);-o-transform: rotate(180deg);transform: rotate(180deg);";
+        referencesCont.style.display = "block";
+    }
+    else {
+        referencesImg.style.cssText -= "-webkit-transform:rotate(180deg);-moz-transform: rotate(180deg);-ms-transform: rotate(180deg);-o-transform: rotate(180deg);transform: rotate(180deg);";
+        referencesCont.style.display = "none";
+    }
+
+    referencesAreExpanded = !referencesAreExpanded;
+}
+var menuIsExpanded = true;
+function menuMobAnimation () {
+    if (menuIsExpanded) {
+        menuMob.classList.remove("menu-mob-out");
+        menuMob.classList.add("menu-mob-in");
+        menuIconOpen.classList.remove("menu-icon-open-out");
+        menuIconOpen.classList.add("menu-icon-open-in");
+        menuIconClose.classList.remove("menu-icon-close-out");
+        menuIconClose.classList.add("menu-icon-close-in");
+    }
+    else {
+        menuMob.classList.remove("menu-mob-in");
+        menuMob.classList.add("menu-mob-out")
+        menuIconClose.classList.remove("menu-icon-close-in");
+        menuIconClose.classList.add("menu-icon-close-out");
+        setTimeout (function () {
+            menuIconOpen.classList.remove("menu-icon-open-in");
+            menuIconOpen.classList.add("menu-icon-open-out");
+        }, 500)
+    }
+    menuIsExpanded = !menuIsExpanded;
+}
